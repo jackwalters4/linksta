076da0fe613255a289319fbca0a9f3315c581797
Jack Walters <jackwalters784@gmail.com>
2022-07-11 17:04:14 -1000

    get a specific category by id

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -4,6 +4,33 @@ var router = express.Router();
 const MongoClient = require('../mongodb');
 const categoriesModel = require('../models/categories');
 
+/** Get category from category id */
+router.get('/', async function(req, res, next) {
+
+    const id = req.query.id;
+
+    const mongo = MongoClient();
+
+    try {
+        await mongo.connect();
+        console.log('connected');
+        const collection = mongo.db('linksta').collection('categories');
+
+        const category = await collection.findOne({"_id" : id});
+        console.log(category);
+
+
+        res.status(200);
+        res.json(category);
+    } catch (err) {
+        res.status(500);
+        res.json({"err": err});
+    } finally {
+        mongo.close();
+    }
+
+})
+
 /** Create a new category for a user given uid */
 router.post('/', async function(req, res, next) {
 
