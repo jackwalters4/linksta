d61173ed74f70f7765303806935557ee64319a15
Jack Walters <jackwalters784@gmail.com>
2022-07-19 23:24:30 -0400

    post link to db

--- frontend/src/Header/AddModal.js
+++ frontend/src/Header/AddModal.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import Close from '@mui/icons-material/Close';
 import './AddModal.css';
 
+/**
+ * Pass in Categories for users as Props -> can use them in dropdown and when link is added
+ */
+
 
 const AddModal = (props) => {
+
+    const [link, setLink] = useState('');
+    const [title, setTitle] = useState('');
+    const [category, setCategory] = useState('');
+    const [notes, setNotes] = useState('');
+
+    // when button is clicked
+    const handleSubmit = async (event) => {
+
+        event.preventDefault();
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' ,
+            'Accept': 'application/json'},
+            body: JSON.stringify({
+                uid: "62ccc3518f2bb12d96456479",
+                category_id: "ofnweifjwoeifnweoifnweoifn",
+                title: title,
+                url: link,
+                note: notes	})
+        }
+
+        const response = await fetch('http://localhost:8000/links', requestOptions);
+        console.log(response);
+    }
+
+
+
     return (
         <>{props.showModal ? (
             <div className='modal-background'>
@@ -15,10 +48,10 @@ const AddModal = (props) => {
                         </IconButton>
                         <h2 className='form-header'>Add Link: </h2>
                     </div>
-                    <form className='form-body'>
-                        <input placeholder='paste link here:' className='input-text' type="text"></input><br/><br/>
-                        <input placeholder='link title' className='input-text' type="text"></input><br/><br/>
-                        <input placeholder='category' className='input-text' type="text"></input><br/><br/>
+                    <form className='form-body' onSubmit={handleSubmit}>
+                        <input placeholder='paste link here:' className='input-text' type="text" value={link || ""} onChange={event => setLink(event.target.value)}></input><br/><br/>
+                        <input placeholder='link title' className='input-text' type="text" value={title || ""} onChange={event => setTitle(event.target.value)}></input><br/><br/>
+                        <input placeholder='category' className='input-text' type="text" value={category || ""} onChange={event => setCategory(event.target.value)}></input><br/><br/>
                         <input className='submit-button' type="submit" value="Submit"></input>
                     </form>
                 </div>
