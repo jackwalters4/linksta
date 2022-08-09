1d739a7f3991874f632ce56d70a9e1ac45f39b8f
Jack Walters <jackwalters784@gmail.com>
2022-08-09 12:04:15 -0400

    api endpoint for delete category

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -98,4 +98,42 @@ router.post('/', async function(req, res, next) {
 
 });
 
+/** Delete a category and all links inside of it */
+router.delete('/', async function(req, res, next) {
+
+    const category_id = req.query.category_id;
+
+    const mongo = MongoClient();
+
+    try {
+        await mongo.connect();
+        console.log('connecteed');
+
+        const catCollection = mongo.db('linksta').collection('categories');
+        const linkCollection = mongo.db('linksta').collection('links');
+
+        const deleteCategory = await catCollection.deleteOne({_id: category_id});
+
+        // if category deletion works, delete links within category
+        if (deleteCategory.deletedCount === 1) {
+            console.log("Successfully deleted category.");
+            const deleteCatLinks = await linkCollection.deleteMany({category_id: category_id});
+            console.log(`Successfully deleted ${deleteCatLinks.deletedCount} links from the deleted category`);
+            res.status(200);
+            res.json({'greeat':'success'});
+        } else {
+            console.log("No categories matched the query. Deleted 0 categories.");
+            res.status(500);
+            res.json({'not great': 'success'});
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500);
+        res.json({"err": err});
+    } finally {
+        mongo.close();
+    }
+
+});
+
 module.exports = router;
